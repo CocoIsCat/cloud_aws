455b6a5d82d58d6cded3855189c3d8a9f1324772
cocofarm <demy176@naver.com>
2021-12-03 04:03:34 +0900

    1번 간소화 및 2번 기능 추가

--- cloud_aws.py
+++ cloud_aws.py
@@ -3,34 +3,30 @@ client = boto3.client('ec2')
 ec2 = boto3.resource('ec2')
 instance = ec2.Instance('id')
 
-### list_instance 함수
+### 1번.list_instance 함수
+
 def list_instance() :
 	instance_ids = []
 	number = 1
-	response = client.describe_instances(Filters=[{'Name': 'instance-type', 'Values': ["t2.micro", "t3.micro"]}])
+	response = client.describe_instances()
 	instances_full_details = response['Reservations']
 	for instance_detail in instances_full_details:
         	group_instances = instance_detail['Instances']
-
         	for instance in group_instances:
-            		instance_id = instance['InstanceId']
-            		instance_ids.append("[id] " + instance_id)
-            		image_id = instance['ImageId']
-            		instance_ids.append("[AMI] " + image_id)
-            		instance_type = instance['InstanceType']
-            		instance_ids.append("[type] " + instance_type)
-            		instance_state = instance['State']
-            		instance_ids.append("[state] " + instance_state['Name'])
-            		instance_monitor = instance['Monitoring']
-            		instance_ids.append("[monitoring state] " + instance_monitor['State'])
-	print("")
-	print("")
-	print("=================================")
-	for i in instance_ids:
-		print(i)
-		if number % 5 == 0:
-			print("=================================")
-		number += 1
+        		print("[id] " + instance['InstanceId'], "[AMI] " + instance['ImageId'], "[type] " + instance['InstanceType'],
+        		 "[state] " + instance['State']['Name'], "[monitoring state] " + instance['Monitoring']['State'])
+            		
+		
+### 2번. available_zones 함수
+def available_zones():
+	response = client.describe_availability_zones()
+	for zone in response['AvailabilityZones']:
+		print("[id] " + zone['ZoneId'], "[region] " + zone['RegionName'], "[zone] " + zone['ZoneName'])
+			
+	
+		
+		
+		
 ### 메인 함수###
 if __name__ == '__main__':
 	number = 0
@@ -53,9 +49,13 @@ if __name__ == '__main__':
 		
 		if number == 1:
 			list_instance()
-
+			
+		elif number == 2:
+			available_zones()
+			
 		elif number == 3:
 			start_instance()
+			
 		elif number == 7:
 			reboot_instance()
 		
