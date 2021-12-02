b655cdb4f0db4a680603694e5bb1352178679ee4
cocofarm <demy176@naver.com>
2021-12-03 03:21:21 +0900

    1번 메뉴 구현

--- cloud_aws.py
+++ cloud_aws.py
@@ -1,5 +1,37 @@
 import boto3
+client = boto3.client('ec2')
+ec2 = boto3.resource('ec2')
+instance = ec2.Instance('id')
 
+### list_instance 함수
+def list_instance() :
+	instance_ids = []
+	number = 1
+	response = client.describe_instances(Filters=[{'Name': 'instance-type', 'Values': ["t2.micro", "t3.micro"]}])
+	instances_full_details = response['Reservations']
+	for instance_detail in instances_full_details:
+        	group_instances = instance_detail['Instances']
+
+        	for instance in group_instances:
+            		instance_id = instance['InstanceId']
+            		instance_ids.append("[id] " + instance_id)
+            		image_id = instance['ImageId']
+            		instance_ids.append("[AMI] " + image_id)
+            		instance_type = instance['InstanceType']
+            		instance_ids.append("[type] " + instance_type)
+            		instance_state = instance['State']
+            		instance_ids.append("[state] " + instance_state['Name'])
+            		instance_monitor = instance['Monitoring']
+            		instance_ids.append("[monitoring state] " + instance_monitor['State'])
+	print("")
+	print("")
+	print("=================================")
+	for i in instance_ids:
+		print(i)
+		if number % 5 == 0:
+			print("=================================")
+		number += 1
+### 메인 함수###
 if __name__ == '__main__':
 	number = 0
 	while True:
@@ -18,3 +50,12 @@ if __name__ == '__main__':
 		print("                                 99. quit                   ")
 		print("------------------------------------------------------------")
 		number = int(input("Enter an integer:"))
+		
+		if number == 1:
+			list_instance()
+
+		elif number == 3:
+			start_instance()
+		elif number == 7:
+			reboot_instance()
+		
